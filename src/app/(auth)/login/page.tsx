daa730fe3ffe619246666769d673704a24519827
gideon-del <gideonchidi471@gmail.com>
2024-10-19 23:19:37 +0100

    chore: login desktop design done

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,14 +1,54 @@
+"use client";
 import Image from "next/image";
 import logo from "@/assets/images/logo.svg";
-
+import illustration from "@/assets/images/illustration.png";
+import "./style.scss";
+import React from "react";
 const LoginPage = () => {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
   return (
     <main className="login__container">
-      <header>
+      <section className="login__header">
         <figure>
           <Image src={logo} alt="Lendsqr" />
         </figure>
-      </header>
+        <figure>
+          <Image src={illustration} alt="illustration" />
+        </figure>
+      </section>
+      <section className="login__main">
+        <form onSubmit={handleSubmit}>
+          <h1 className="login__title">Welcome!</h1>
+          <p className="login__desc">Enter details to login.</p>
+          <fieldset className="login__input--wrapper">
+            <div className="login__input--container">
+              <input
+                type="email"
+                className="login__input"
+                placeholder="Email"
+              />
+            </div>
+            <div className="login__input--container">
+              <input
+                type="password"
+                className="login__input"
+                placeholder="Password"
+              />
+              <button className="show__btn" type="button">
+                SHOW
+              </button>
+            </div>
+          </fieldset>
+          <button type="button" className="forgot__password__btn">
+            Forgot PASSWORD?
+          </button>
+          <button type="submit" className="login__btn">
+            Log in
+          </button>
+        </form>
+      </section>
     </main>
   );
 };
